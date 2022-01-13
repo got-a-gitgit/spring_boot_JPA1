5463e85d4491616ddee828691691993a433475f2
got-a-gitgit <soo_1992@naver.com>
2022-01-13 14:42:46 +0900

    View

---
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @GetMapping("/")
+    String home(){
+        log.info("home controller");
+        return "home";
+    }
+}
